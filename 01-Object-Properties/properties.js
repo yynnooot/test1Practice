27b1a5ca00adeebfdde9ad7aefdbe18c082543ce
Tony Yoon <tooonnyy@gmail.com>
2017-08-19 16:38:37 -0400

    Completed

---
+++ 01-Object-Properties/properties.js
@@ -0,0 +1,30 @@
+function setPropsOnObj(obj){
+    obj.x = 7;
+    obj.y = 8;
+    obj.onePlus = function(num){
+        return num+1;
+    }
+}
+
+function setPropsOnArr(arrObj){
+    arrObj.hello = function(){
+        return "Hello!";
+    }
+    arrObj.full = "stack";
+    arrObj[0] = 5;
+    arrObj.twoTimes = function(arg){
+        return arg*2;
+    }
+}
+
+function setPropsOnFunc(funcObj){
+    funcObj();
+    funcObj.year = '20??';
+    funcObj.divideByTwo = function(num){
+        return num/2;
+    }
+    funcObj.prototype.helloWorld = function(){
+        return 'Hello World';
+    }
+}
+
